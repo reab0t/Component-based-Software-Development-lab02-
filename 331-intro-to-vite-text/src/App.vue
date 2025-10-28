0d4ff6e6ba4b2ab96b90cac8ed4aea96b95acf7e
JinLei <15560674459@163.com>
2025-10-28 22:58:59 +0800

    lab04 step5

--- 331-intro-to-vite-text/src/App.vue
+++ 331-intro-to-vite-text/src/App.vue
@@ -10,12 +10,16 @@ const {message} = storeToRefs(store)
 </script>
 
 <template>
+  <SpeedInsights/>
 
     <div class="text-center font-sans text-gray-700 antialias">
       <header>
         <div id="flashMessage" class="animate-fade" v-if="message">
           <h4>{{message}}</h4>
         </div>
+        <h1>
+          Deploy with Vercel
+        </h1>
         <div class="wrapper">
           <nav class="py-6">
             <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">Event</RouterLink> |
